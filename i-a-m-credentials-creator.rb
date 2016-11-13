b4af707aedb210843ed0be0b793bdb39940d92f7
cundo <cundo@hotmail.com>
2016-11-13 15:58:36 -0800

    fixed install path

--- i-a-m-credentials-creator.rb
+++ i-a-m-credentials-creator.rb
@@ -6,8 +6,8 @@ class IAMCredentialsCreator < Formula
   sha256 '232fa18d7a92d95bc32161de3c2e9c9fc131cb8b6a28d55ccaff3029d24170f0'
 
   def install
-    bin.install "create_iam_accounts.py"
-    mv "#{bin}/create_iam_accounts.py", "#{bin}/smartiamcreator"
+    bin.install "formula-smartiamcreator/create_iam_accounts.py"
+    mv "#{bin}/formula-smartiamcreator/create_iam_accounts.py", "#{bin}/smartiamcreator"
   end
 
   def test
